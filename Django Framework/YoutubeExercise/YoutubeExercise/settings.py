3d1156cae12acd8a10d05dd88d73ba01478bda7e
Mario Georgiev <90646386+MarioNG21@users.noreply.github.com>
2022-03-06 20:24:34 +0200

    Update settings.py

--- Django Framework/YoutubeExercise/YoutubeExercise/settings.py
+++ Django Framework/YoutubeExercise/YoutubeExercise/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(5+1@7rt4fbpw8_v9st#_^m^$juun7593^c=o#s4k$3pohmrl&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
